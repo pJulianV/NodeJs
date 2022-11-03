eb39d9fdabff3665a147546dfabae8178001cf5a
Julian Vargas <julianvargastrb@gmail.com>
2022-11-02 22:16:03 -0500

    Servidor con node

--- ServidorNode/routes/routes.js
+++ ServidorNode/routes/routes.js
@@ -3,19 +3,19 @@ import express from 'express';
 
 export const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     res.send('Esta es la raiz ☀️')
 })
 
-router.get('/login', function (req, res) {
+router.get('/login', (req, res) => {
     res.send('Esta es el login ☀️')
 })
 
-router.get('/add', function (req, res) {
+router.get('/add', (req, res) => {
     res.send('Añades.. ? ☀️')
 })
 
-router.get('/delete', function (req, res) {
+router.get('/delete', (req, res) => {
     res.send('Borras ☀️')
 })
 
